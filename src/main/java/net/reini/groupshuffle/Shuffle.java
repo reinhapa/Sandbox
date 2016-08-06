7b4abf3d04d7eb30b5f5dad3442023418a97d834
Patrick Reinhart <patrick@reini.net>
2016-08-06 22:35:48 +0200

    Adds latest changes

--- src/main/java/net/reini/groupshuffle/Shuffle.java
+++ src/main/java/net/reini/groupshuffle/Shuffle.java
@@ -1,15 +1,17 @@
 package net.reini.groupshuffle;
 
+import java.util.function.IntConsumer;
+
 import javafx.application.Application;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -26,15 +28,17 @@ public class Shuffle extends Application {
     editBtn.setMinWidth(50);
     TextArea text = new TextArea();
 
-    HBox buttons = new HBox(5);
-    buttons.setPadding(new Insets(5));
-    buttons.setAlignment(Pos.CENTER);
-
-    ObservableList<Node> children = buttons.getChildren();
+    HBox hbox = new HBox(5);
+    hbox.setPadding(new Insets(5));
+    hbox.setAlignment(Pos.CENTER_LEFT);
+    ScrollPane buttons = new ScrollPane(hbox);
+    buttons.setVbarPolicy(ScrollBarPolicy.NEVER);
+    buttons.setHbarPolicy(ScrollBarPolicy.NEVER);
+    ObservableList<Node> children = hbox.getChildren();
     children.add(editBtn);
     children.add(new Label("Gruppen:"));
     for (int i = 2; i < 6; i++) {
-      Button shuffleBtn = new Button(Integer.toString(i));
+      Button shuffleBtn = new ShuffleButton(i, value -> text.textProperty().set("groups" + value));
       shuffleBtn.setMinWidth(50);
       children.add(shuffleBtn);
     }
@@ -42,16 +46,16 @@ public class Shuffle extends Application {
     BorderPane root = new BorderPane();
     root.setTop(buttons);
     root.setCenter(text);
-
-    editBtn.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(ActionEvent event) {
-        text.textProperty().set(text.getText().concat("Hello World!\n"));
-      }
-    });
-
+    editBtn.setOnAction(event -> text.textProperty().set(text.getText().concat("Hello World!\n")));
     primaryStage.setTitle("Shuffle");
     primaryStage.setScene(new Scene(root, 300, 250));
     primaryStage.show();
+  }
+
+  class ShuffleButton extends Button {
+    ShuffleButton(int groups, IntConsumer shuffleAction) {
+      super(String.valueOf(groups));
+      setOnAction(event -> shuffleAction.accept(groups));
+    }
   }
 }
