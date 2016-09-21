9d526a8a1fc29f8736c1f2ad50e074959f06e206
Patrick Reinhart <patrick@reini.net>
2016-09-21 23:20:59 +0200

    Adds Timer alternative sample code

---
+++ src/main/java/net/reini/BeeperControl.java
@@ -0,0 +1,45 @@
+package net.reini;
+
+import static java.util.concurrent.TimeUnit.HOURS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+class BeeperControl {
+	private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+	public static void main(String[] args) {
+		try {
+			new BeeperControl().beepForAnHour();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void beepForAnHour() throws Exception {
+		final Runnable beeper = new Runnable() {
+			@Override
+			public void run() {
+				System.out.println("beep");
+			}
+		};
+		final ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(beeper, 10, 10, SECONDS);
+		scheduler.schedule(new Runnable() {
+			@Override
+			public void run() {
+				beeperHandle.cancel(true);
+			}
+		}, 1, HOURS);
+
+		TimeUnit.SECONDS.sleep(30);
+		
+		System.out.println("cancel beeper "+beeperHandle.cancel(true));
+		System.out.println("beeper canceled " + beeperHandle.isCancelled());
+		System.out.println("beeper done " + beeperHandle.isDone());
+		TimeUnit.SECONDS.sleep(10);
+
+	}
+}
