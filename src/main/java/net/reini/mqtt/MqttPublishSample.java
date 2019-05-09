e3d8410748716447d7d85c5ad6d10f1f5eb8ae24
Patrick Reinhart <patrick@reini.net>
2019-05-09 22:51:30 +0200

    Adds OctoPrint subscription

--- src/main/java/net/reini/mqtt/MqttPublishSample.java
+++ src/main/java/net/reini/mqtt/MqttPublishSample.java
@@ -1,8 +1,9 @@
 package net.reini.mqtt;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.util.Optional;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
@@ -13,18 +14,17 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 public class MqttPublishSample {
 
   public static void main(String[] args) {
-
+    Properties systemProperties = System.getProperties();
     String topic = "MQTT Examples";
     String content = "Message from MqttPublishSample";
+    String clientId = "JavaSample";
     int qos = 2;
     // String broker = "tcp://iot.eclipse.org:1883";
-    String broker = "tcp://127.0.0.1:1883";
-    String clientId = "JavaSample";
+    String broker = systemProperties.getProperty("mqtt.url", "tcp://127.0.0.1:1883");
     MemoryPersistence persistence = new MemoryPersistence();
 
-    Properties systemProperties = System.getProperties();
 
-    try (MqttClient sampleClient = new MqttClient(broker, clientId, persistence)) {
+    try (MqttClient sampleClient = new MqttClient(broker, clientId, persistence); BufferedInputStream console = new BufferedInputStream(System.in)) {
       MqttConnectOptions connOpts = new MqttConnectOptions();
       connOpts.setCleanSession(true);
       Optional.ofNullable(systemProperties.getProperty("mqtt.username"))
@@ -41,6 +41,11 @@ public class MqttPublishSample {
           (t, m) -> System.out.format("OwnTracks: topic: %s, message: %s%n", t,
               new String(m.getPayload())));
 
+      System.out.println("Subscribing to OctoPrint events");
+      sampleClient.subscribe("octoprint/#",
+          (t, m) -> System.out.format("OctoPrint: topic: %s, message: %s%n", t,
+              new String(m.getPayload())));
+
       System.out.println("Subscribing to '" + topic + "' events");
       sampleClient.subscribe(topic,
           (t, m) -> System.out.format("All Messages: topic: %s, message: %s%n", t,
@@ -52,10 +57,13 @@ public class MqttPublishSample {
       sampleClient.publish(topic, message);
       System.out.println("Message published");
 
-      TimeUnit.MINUTES.sleep(1);
-      sampleClient.disconnect();
-      System.out.println("Disconnected");
-      System.exit(0);
+      for (;;) {
+        if ('q' == console.read()) {
+          sampleClient.disconnect();
+          System.out.println("Disconnected");
+          System.exit(0);
+        }
+      }
     } catch (MqttException me) {
       System.out.println("reason " + me.getReasonCode());
       System.out.println("msg " + me.getMessage());
@@ -63,7 +71,7 @@ public class MqttPublishSample {
       System.out.println("cause " + me.getCause());
       System.out.println("excep " + me);
       me.printStackTrace();
-    } catch (InterruptedException e) {
+    } catch (IOException e) {
       e.printStackTrace();
     }
   }
